08226da2628f8fc69ff37182479177edf4f782a1
Tommi Cox <tommicox@gmail.com>
2019-03-05 16:01:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def long_planeteer_calls(short_words)
 end
 
 def find_the_cheese(cheese_types)
-  cheese_types = % (cheddar, gouda, camembert)
+  cheese_types = %(cheddar, gouda, camembert)
   cheese_types.include?(cheese_types) {|cheese| cheese.include}
 end
