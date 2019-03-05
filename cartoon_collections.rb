ad053da6b9cd0b841284a8023d8b029f729918ad
Tommi Cox <tommicox@gmail.com>
2019-03-05 16:05:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = %(cheddar, gouda, camembert)
   cheese_types.find do |cheese_types|
-    cheese_types.include
+    cheese_types.include 
 end
 end
